3c0c2f5625bcbb7bb143665bc9ec0ee1f00bf788
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2020-09-22 17:20:27 -0300

    *

--- resources/views/site/pages/contact.blade.php
+++ resources/views/site/pages/contact.blade.php
@@ -4,10 +4,11 @@
 
 @section('content')
 
-<section>
-    <div class="contact">
-        <div class="contact__items">
-            @if($contact)
+@if($contact)
+
+    <section>
+        <div class="contact">
+            <div class="contact__items">            
                 <div class="contact__image">
                     <img src="{{ URL::asset('storage/' . (isMobile() && $contact['image_mobile'] ? $contact['image_mobile'] : $contact['image'])) }}" alt="{{ $contact['title'] }}" title="{{ $contact['title'] }}" />
                 </div>
@@ -17,10 +18,11 @@
                     <br/>
                     <p>{!! nl2br($contact['description']) !!}</p>
                 </div>
-            @endif
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+
+@endif
 
 @include('site/includes/form', ['class' => 'form-contact'])
 
